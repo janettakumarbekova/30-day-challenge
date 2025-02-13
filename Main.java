2833336f921e2073bf00d69a1b960e2015e5d566
janettadev <kumarbekovajanetta@gmail.com>
2025-02-13 22:53:22 +0600

    array

---
+++ Main.java
@@ -0,0 +1,32 @@
+// my first branch
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         int age = 25;
+//         double price = 19.88;
+//         char letter = 'A'; 
+//         boolean isJava = true;
+
+//         int num = 10;
+//         if (num > 0) {
+//             System.out.println("Положительное число");
+//         } else {
+//             System.out.println("Отрицательное число");
+//         }
+//     }
+// }
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5}; 
+        System.out.println(numbers.length); 
+    }
+}
+
+
+
